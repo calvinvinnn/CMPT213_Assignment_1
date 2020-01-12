c82eabfa801ec56cc07be36933547f0fe698630b
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-01-11 21:38:38 -0800

    Finished coding option 5 (debug)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,33 +2,80 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.System.exit;
+import static java.lang.System.setOut;
 
 public class Main {
-    ArrayList<MinionInformation> minion = new ArrayList<MinionInformation>();
+    static ArrayList<MinionInformation> minion = new ArrayList<MinionInformation>();
     static Menu menu = new Menu();
     public static void main(String[] args){
         System.out.println("Welcome to the Evil Minion Tracker");
         System.out.println("by Christofer Calvin Kurniawan");
-        menu.printMenu();
+        Scanner info = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         String st = "-1";
         while(st.compareTo("0") != 0) {
-            Scanner input = new Scanner(System.in);
+            menu.printMenu();
             st = input.nextLine();
             if (st.compareTo("1") == 0) {
-                System.out.println("a");
+                if(minion.size() == 0){
+                    System.out.println("No minion listed!");
+                    continue;
+                }
+                for(int i=0; i<minion.size(); i++){
+                    System.out.println(i+1 + ". "
+                            + minion.get(i).getName() + ", "
+                            + minion.get(i).getHeight() + "m, "
+                            + minion.get(i).getNumOfEvilDeeds() + " evil deed(s)");
+                }
             } else if (st.compareTo("2") == 0) {
-                System.out.println("b");
+                String name, height;
+                System.out.println("Enter minion's name : ");
+                name = info.nextLine();
+                System.out.println("Enter minion's height : ");
+                height = info.nextLine();
+                while(height.compareTo("0") == 0){
+                    System.out.println("Enter a correct number.");
+                    height = info.nextLine();
+                }
+                minion.add (new MinionInformation(name, Double.parseDouble(height), 0));
+                continue;
             } else if (st.compareTo("3") == 0) {
 
             } else if (st.compareTo("4") == 0) {
+                if(minion.size() == 0){
+                    System.out.println("No minion listed!");
+                    continue;
+                }
+                for(int i=0; i<minion.size(); i++){
+                    System.out.println(i+1 + ". "
+                            + minion.get(i).getName() + ", "
+                            + minion.get(i).getHeight() + "m, "
+                            + minion.get(i).getNumOfEvilDeeds() + " evil deed(s)");
+                }
+                System.out.println("(Enter 0 to cancel)");
+
+                st = input.nextLine();
+                while(Integer.valueOf(st) >= minion.size() || Integer.valueOf(st) <= 0){
+                    if(st.compareTo("0") == 0){
+                        break;
+                    }
+                    System.out.println("Choose an available option.");
+                    st = input.nextLine();
+                }
+                //how to change numevildeeds
 
             } else if (st.compareTo("5") == 0) {
-
+                System.out.println(minion.toString());
             } else if (st.compareTo("6") == 0) {
                 exit(0);
             } else {
                 System.out.println("Choose an available option.");
             }
+            while(st.compareTo("0") != 0){
+                System.out.println("(Enter 0 to cancel)");
+                st = input.nextLine();
+            }
+            st = "reset";
         }
     }
 
--- src/main/java/MinionInformation.java
+++ src/main/java/MinionInformation.java
@@ -3,11 +3,28 @@ public class MinionInformation {
     private double height;
     private int NumOfEvilDeeds;
 
+    public void setNumOfEvilDeeds(int numOfEvilDeeds) {
+        NumOfEvilDeeds = numOfEvilDeeds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getNumOfEvilDeeds() {
+        return NumOfEvilDeeds;
+    }
+
     public MinionInformation(String name, double height, int numOfEvilDeeds) {
         this.name = name;
         this.height = height;
         NumOfEvilDeeds = numOfEvilDeeds;
     }
+
 
     @Override
     public String toString() {
