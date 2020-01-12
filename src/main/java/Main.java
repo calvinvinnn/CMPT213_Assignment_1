6321242d6f7d7e972641bb51b6223b572be0e0d3
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-01-11 17:46:52 -0800

    Added print for main menu and the menu list Added user input Finished coding option 6 (exit)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 public class Main {
+    ArrayList<MinionInformation> minion = new ArrayList<MinionInformation>();
+    static Menu menu = new Menu();
     public static void main(String[] args){
+        System.out.println("Welcome to the Evil Minion Tracker");
+        System.out.println("by Christofer Calvin Kurniawan");
+        menu.printMenu();
+        String st = "-1";
+        while(st.compareTo("0") != 0) {
+            Scanner input = new Scanner(System.in);
+            st = input.nextLine();
+            if (st.compareTo("1") == 0) {
+                System.out.println("a");
+            } else if (st.compareTo("2") == 0) {
+                System.out.println("b");
+            } else if (st.compareTo("3") == 0) {
 
+            } else if (st.compareTo("4") == 0) {
+
+            } else if (st.compareTo("5") == 0) {
+
+            } else if (st.compareTo("6") == 0) {
+                exit(0);
+            } else {
+                System.out.println("Choose an available option.");
+            }
+        }
     }
-    ArrayList<MinionInformation> minion = new ArrayList<MinionInformation>;
-    if (minion.isEmpty()){
-        System.out.println("No Minion Listed");
-    }
-
-
 
 
 }
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,7 +1,17 @@
 public class Menu {
-    private String MenuTitle;
-    private String[] MenuOptions;
-    private void printMenu(String Menutitle, String[] MenuOptions){
+    private static String MenuTitle = "Main Menu";
+    private static String[] MenuOptions = {"List Minions",
+                                    "Add a new minion",
+                                    "Remove a minion",
+                                    "Attribute evil deed to a minion",
+                                    "DEBUG: Dump objects (toString)",
+                                    "Exit"};
 
+
+    public static void printMenu(){
+        System.out.println(MenuTitle);
+        for(int i=1; i<7; i++){
+            System.out.println(i + ". " + MenuOptions[i-1]);
+        }
     }
 }
