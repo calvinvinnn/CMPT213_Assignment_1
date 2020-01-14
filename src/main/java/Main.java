bfb4e9760185a291670095ab7c172c4f1dea4da2
calvinvinnn <calvinvinnn@users.noreply.github.com>
2020-01-14 00:03:09 -0800

    Replace ifelse with SwitchCase Add cancel_Enter_0 function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,61 +17,80 @@ public class Main {
 
         while (true) {
             menu.printMenu();
-            st = input.nextLine();
-            if (st.compareTo("1") == 0) {
-                noMinionListedCheck();
-                printMinionList();
-                actionIsDone();
-            } else if (st.compareTo("2") == 0) {
-                String name, height;
-                System.out.println("Enter minion's name : ");
-                name = info.nextLine();
-                System.out.println("Enter minion's height : ");
-                height = info.nextLine();
-                while (height.compareTo("0") == 0) {
-                    System.out.println("Enter a correct number.");
-                    height = info.nextLine();
-                }
-                minion.add(new MinionInformation(name, Double.parseDouble(height), 0));
-                System.out.println("New minion added to the list.");
-                actionIsDone();
-            } else if (st.compareTo("3") == 0) {
-                if (minion.size() == 0) {
-                    System.out.println("No minion listed!");
-                    continue;
-                }
-                printMinionList();
-                System.out.println("(Enter 0 to cancel)");
+            while (true) {
                 st = input.nextLine();
-                wrongChoiceCheck();
-                minion.remove(Integer.valueOf(st)-1);
-                System.out.println("Removed !");
-                actionIsDone();
-            } else if (st.compareTo("4") == 0) {
-                if (minion.size() == 0) {
-                    System.out.println("No minion listed!");
-                    continue;
-                }
-                printMinionList();
-                System.out.println("(Enter 0 to cancel)");
-                st = input.nextLine();
-                wrongChoiceCheck();
-                minion.get(Integer.valueOf(st)-1).incrementEvilDeeds();
-                System.out.println("Increased the number of evil deeds by 1.");
-                actionIsDone();
-            } else if (st.compareTo("5") == 0) {
-                System.out.println(minion.toString());
-                actionIsDone();
-            } else if (st.compareTo("6") == 0) {
-                exit(0);
-            } else {
+                int optionCheck = Integer.valueOf(st);
+                if (optionCheck > 0 && optionCheck < 7)
+                    break;
                 System.out.println("Choose an available option.");
             }
-
+            switch (st) {
+                case "1":
+                    noMinionListedCheck();
+                    printMinionList();
+                    actionIsDone();
+                    break;
+                case "2":
+                    String name, height;
+                    System.out.println("Enter minion's name : ");
+                    name = info.nextLine();
+                    System.out.println("Enter minion's height : ");
+                    height = info.nextLine();
+                    while (Double.valueOf(height) <= 0) {
+                        System.out.println("Enter a correct number.");
+                        height = info.nextLine();
+                    }
+                    minion.add(new MinionInformation(name, Double.parseDouble(height), 0));
+                    System.out.println("New minion added to the list.");
+                    actionIsDone();
+                    break;
+                case "3":
+                    if (minion.size() == 0) {
+                        System.out.println("No minion listed!");
+                        while (st.compareTo("0") != 0) {
+                            cancel_Enter_0();
+                        }
+                        continue;
+                    }
+                    printMinionList();
+                    cancel_Enter_0();
+                    wrongChoiceCheck();
+                    minion.remove(Integer.valueOf(st) - 1);
+                    System.out.println("Removed !");
+                    actionIsDone();
+                    break;
+                case "4":
+                    if (minion.size() == 0) {
+                        System.out.println("No minion listed!");
+                        while (st.compareTo("0") != 0) {
+                            cancel_Enter_0();
+                        }
+                        continue;
+                    }
+                    printMinionList();
+                    cancel_Enter_0();
+                    wrongChoiceCheck();
+                    minion.get(Integer.valueOf(st) - 1).incrementEvilDeeds();
+                    System.out.println("Increased the number of evil deeds by 1.");
+                    actionIsDone();
+                    break;
+                case "5":
+                    System.out.println(minion.toString());
+                    actionIsDone();
+                    break;
+                case "6":
+                    exit(0);
+            }
         }
     }
 
-    public static void printMinionList(){
+
+    public static void cancel_Enter_0() {
+        System.out.println("(Enter 0 to cancel)");
+        st = input.nextLine();
+    }
+
+    public static void printMinionList() {
         for (int i = 0; i < minion.size(); i++) {
             System.out.println(i + 1 + ". "
                     + minion.get(i).getName() + ", "
@@ -80,7 +99,7 @@ public class Main {
         }
     }
 
-    public static void wrongChoiceCheck(){
+    public static void wrongChoiceCheck() {
         while (Integer.valueOf(st) > minion.size() || Integer.valueOf(st) <= 0) {
             if (st.compareTo("0") == 0) {
                 break;
@@ -90,17 +109,18 @@ public class Main {
         }
     }
 
-    public static void noMinionListedCheck(){
+    public static void noMinionListedCheck() {
         if (minion.size() == 0) {
             System.out.println("No minion listed!");
         }
     }
 
-    public static void actionIsDone(){
+    public static void actionIsDone() {
         while (st.compareTo("0") != 0) {
             System.out.println("(Enter 0 to cancel)");
             st = input.nextLine();
         }
+
     }
 
 
